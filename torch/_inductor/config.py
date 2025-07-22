55d06112005ac93cc0bd62ec3cbb94f2d94b163e
Ruben Rodriguez Buchillon <coconutruben@fb.com>
2025-07-22 01:23:41 -0700

    [inductor] add template hashing for template lookup table
    \# Why
    
    Make it possible for table users to specify a hash and
    be more conservative with source code changes and table validity
    
    \# What
    
    - TritonTemplate stores a src hash
    - Lookup table queries can take a src hash
    - If
      - The template has a hash (Triton)
      - The user provides it inside their lookup table entry
      - The hash does not match at runtime
      Then the table has become stale and we filter out that entry
    
    Test Plan:
    
    - added tests to cover full behavior in test_lookup_table.py
    - added tests to cover e2e behaviors in test_lookup_table_e2e.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 4660a467db23ed03883307e7c34fbc17dab7254a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/158091

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -1862,6 +1862,12 @@ external_matmul: list[Callable[[torch.Tensor, torch.Tensor, torch.Tensor], None]
 # - template_id: Identifier for the template (e.g., "mm", "tma", "decompose_k")
 # - Various template-specific parameters (BLOCK_M, BLOCK_N, etc.)
 #
+# If you want to make sure your lookup table is as stable as possible, you can
+# add 'template_hash' with a hash of the source code for templates that support it
+# e.g. TritonTemplate. The Inductor logs will print the hash of the template source
+# when using the lookup table. If you add a `template_hash` to the config, and it
+# does not match the template source hash at runtime, the config will be filtered out
+#
 # Example:
 # {
 #   "NVIDIA H100+mm+((torch.float16, [128, 64], [64, 1]), (torch.float16, [64, 128], [1, 64]))": [
