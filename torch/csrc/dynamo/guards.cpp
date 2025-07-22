b8d04c3f947364220b7fe9441f25aba618617074
Animesh Jain <anijain@umich.edu>
2025-07-21 17:19:57 -0700

    [dynamo][guards] Find tag safe nodes to prepare for guard optimizations
    ghstack-source-id: 981ea5c5cdc83e719dc21434a792dd1bb1f07702
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/158803

--- torch/csrc/dynamo/guards.cpp
+++ torch/csrc/dynamo/guards.cpp
@@ -2619,6 +2619,29 @@ class GuardManager {
   }
 
  public:
+  // tag safety related helpers
+  // Seen docstring in guards.py ``find_tag_safe_roots`` for full context
+  void mark_tag_safe() {
+    _is_tag_safe = true;
+  }
+
+  void mark_tag_safe_root() {
+    if (!_is_tag_safe) {
+      throw std::runtime_error(
+          "Marking a node tag_safe_root when its not tag safe");
+    }
+    _is_tag_safe_root = true;
+  }
+
+  bool is_tag_safe() {
+    return _is_tag_safe;
+  }
+
+  bool is_tag_safe_root() {
+    return _is_tag_safe_root;
+  }
+
+ public:
   // For cloning
   GuardManager(
       RootGuardManager* root,
@@ -2673,6 +2696,12 @@ class GuardManager {
         _is_immutable,
         _is_nn_module,
         _type_str);
+    if (is_tag_safe()) {
+      cloned_mgr->mark_tag_safe();
+      if (is_tag_safe_root()) {
+        cloned_mgr->mark_tag_safe_root();
+      }
+    }
     clone_common(cloned_root, cloned_mgr, clone_filter_fn);
     return cloned_mgr;
   }
@@ -2958,6 +2987,10 @@ class GuardManager {
   bool _is_nn_module = false;
   std::string _type_str;
   uint64_t _dict_tag{0};
+
+  // tag safe markers
+  bool _is_tag_safe = false;
+  bool _is_tag_safe_root = false;
 };
 
 GuardAccessor::GuardAccessor(
@@ -3489,7 +3522,12 @@ class DictGuardManager : public GuardManager {
         _indices,
         type_of_guarded_value(),
         is_guarded_value_empty_dict());
-
+    if (is_tag_safe()) {
+      cloned_mgr->mark_tag_safe();
+      if (is_tag_safe_root()) {
+        cloned_mgr->mark_tag_safe_root();
+      }
+    }
     clone_common(cloned_root, cloned_mgr, clone_filter_fn);
     for (auto index : _indices) {
       KeyValueManager& key_value_manager = _key_value_managers[index];
@@ -6012,6 +6050,10 @@ PyObject* torch_c_dynamo_guards_init() {
       .def(
           "is_guarded_value_empty_dict",
           &GuardManager::is_guarded_value_empty_dict)
+      .def("mark_tag_safe", &GuardManager::mark_tag_safe)
+      .def("mark_tag_safe_root", &GuardManager::mark_tag_safe_root)
+      .def("is_tag_safe", &GuardManager::is_tag_safe)
+      .def("is_tag_safe_root", &GuardManager::is_tag_safe_root)
       .def("type_of_guarded_value", &GuardManager::type_of_guarded_value)
       .def(
           "get_accessors",
